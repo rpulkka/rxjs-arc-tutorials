40ff7ed287814e38a5a12b2853db59efb689ce7b
rpulkka <robert.pulkka@gmail.com>
2023-03-16 09:27:30 +0200

    Lesson 5: From-Operator

--- src/app/of-operator/of-operator.component.ts
+++ src/app/of-operator/of-operator.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-of-operator',
@@ -19,6 +19,8 @@ export class OfOperatorComponent {
   }
   student$: Observable<{id: number, name: string}> = of(this.studentObject)
 
+  fromStudents$: Observable<string> = from(this.studentList)
+
   ngOnInit(): void {
     this.students.subscribe(data => {
       console.log(data)
@@ -29,6 +31,12 @@ export class OfOperatorComponent {
     })
 
     this.student$.subscribe(data => {
+      console.log(data)
+    })
+
+    console.log('FROM')
+
+    this.fromStudents$.subscribe(data => {
       console.log(data)
     })
   }
